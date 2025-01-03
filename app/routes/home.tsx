89e0422f7aa87376b9c3ca7c21ddd7f95ada8475
Angelillo15 <contact@angelillo15.es>
2025-01-03 01:03:06 +0100

    meta: update info

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import { Hero } from '@/components/hero';
 import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-    return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
+    return [{ title: 'Nookure Studios' }, { name: 'description', content: 'Welcome to NookureStudios homepage, ' }];
 }
 
 export default function Home() {
