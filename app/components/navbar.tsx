e7db471243fd02c27888ef340c1368953c1154e6
Angelillo15 <contact@angelillo15.es>
2025-01-03 01:02:40 +0100

    ui: change NavBar logic

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, type ReactNode } from 'react';
 import {
     Navbar,
     NavbarBrand,
@@ -8,72 +8,93 @@ import {
     NavbarMenu,
     NavbarMenuItem,
     Link,
-    Button,
 } from '@nextui-org/react';
 import { NookureIcon } from '@/icons/nookure-icon';
 import { Github } from '@/icons/github';
 import { DiscordIcon } from '@/icons/discord';
+import { useLocation, useNavigate } from 'react-router';
+
+interface NavItem {
+    url: string;
+    external?: boolean;
+    label: string;
+    icon?: ReactNode;
+}
+
+const navMenu: NavItem[] = [
+    {
+        url: '/',
+        label: '/home',
+    },
+    {
+        url: '/downloads',
+        label: '/downloads',
+    },
+    {
+        url: 'https://github.com/Nookure',
+        label: '/github',
+        external: true,
+        icon: <Github />,
+    },
+    {
+        url: 'https://discord.nookure.com/',
+        label: '/discord',
+        external: true,
+        icon: <DiscordIcon />,
+    },
+];
 
 export default function App() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const navigation = useNavigate();
+    const location = useLocation();
 
-    const menuItems = [
-        'Profile',
-        'Dashboard',
-        'Activity',
-        'Analytics',
-        'System',
-        'Deployments',
-        'My Settings',
-        'Team Settings',
-        'Help & Feedback',
-        'Log Out',
-    ];
+    useEffect(() => {
+        setIsMenuOpen(false);
+    }, [location.pathname]);
 
     return (
-        <Navbar onMenuOpenChange={setIsMenuOpen} isBlurred={false}>
+        <Navbar onMenuOpenChange={setIsMenuOpen} isMenuOpen={isMenuOpen} isBlurred={false} className='vt-name-[navbar]'>
             <NavbarContent>
                 <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className='sm:hidden' />
-                <NavbarBrand>
+                <NavbarBrand onClick={() => navigation('/', { viewTransition: true })} className='cursor-pointer'>
                     <NookureIcon className='w-6 h-6 mr-2' />
                     <p className='font-bold text-inherit font-mono'>Nookure</p>
                 </NavbarBrand>
             </NavbarContent>
 
             <NavbarContent className='hidden sm:flex gap-4 font-mono' justify='center'>
-                <NavbarItem>
-                    <Link color='foreground' href='#'>
-                        /
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link color='foreground' aria-current='page' href='#'>
-                        /downloads
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color='foreground' href='#'>
-                        <Github className='mr-1'/> /nookure
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color='foreground' href='#'>
-                        <DiscordIcon className='mr-1'/> /nookure
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify='end'>
-            </NavbarContent>
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={`${item}-${index}`}>
+                {navMenu.map((item, index) => (
+                    <NavbarItem key={`${item.url}-${index}`}>
                         <Link
-                            className='w-full'
-                            color={index === 2 ? 'primary' : index === menuItems.length - 1 ? 'danger' : 'foreground'}
-                            href='#'
-                            size='lg'
+                            color='foreground'
+                            className='flex items-center'
+                            href={item.url}
+                            target={item.external ? '_blank' : undefined}
                         >
-                            {item}
+                            {item.icon && <span className='mr-1'>{item.icon}</span>}
+                            {item.label}
+                        </Link>
+                    </NavbarItem>
+                ))}
+            </NavbarContent>
+            <NavbarContent justify='end' />
+            <NavbarMenu>
+                <NavbarMenuItem>Items:</NavbarMenuItem>
+                {navMenu.map((item, index) => (
+                    <NavbarMenuItem key={`${item.url}-${index}`}>
+                        <Link
+                            color='foreground'
+                            className='flex items-center'
+                            href={item.external ? item.url : undefined}
+                            target={item.external ? '_blank' : undefined}
+                            onPress={() => {
+                                
+                                setTimeout(() => navigation(item.url, { viewTransition: true }), 100);
+                            }}
+                        >
+                            {item.icon && <span className='mr-1'>{item.icon}</span>}
+                            {item.label}
                         </Link>
                     </NavbarMenuItem>
                 ))}
