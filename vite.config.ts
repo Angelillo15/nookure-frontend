4f9ef78d9b9e979f0e1b2415e584cbdac1b5c119
Angelillo15 <contact@angelillo15.es>
2025-01-03 01:51:52 +0100

    build: add cloudflare support

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,64 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePluginViteNodeMiniflare } from '@hiogawa/vite-node-miniflare';
+import { reactRouter } from '@react-router/dev/vite';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
-});
+let config;
+
+if (process.env.NODE_ENV === 'production') {
+    config = defineConfig(({ isSsrBuild }) => ({
+        build: {
+            rollupOptions: isSsrBuild
+                ? {
+                      input: './workers/app.ts',
+                  }
+                : undefined,
+        },
+        css: {
+            postcss: {
+                plugins: [tailwindcss, autoprefixer],
+            },
+        },
+        ssr: {
+            target: 'webworker',
+            noExternal: true,
+            resolve: {
+                conditions: ['workerd', 'browser'],
+            },
+            optimizeDeps: {
+                include: [
+                    'react',
+                    'react/jsx-runtime',
+                    'react/jsx-dev-runtime',
+                    'react-dom',
+                    'react-dom/server',
+                    'react-router',
+                ],
+            },
+        },
+        plugins: [
+            vitePluginViteNodeMiniflare({
+                entry: './workers/app.ts',
+                miniflareOptions: (options) => {
+                    options.compatibilityDate = '2024-11-18';
+                    options.compatibilityFlags = ['nodejs_compat'];
+                },
+            }),
+            reactRouter(),
+            tsconfigPaths(),
+        ],
+    }));
+} else {
+    config = defineConfig({
+        css: {
+            postcss: {
+                plugins: [tailwindcss, autoprefixer],
+            },
+        },
+        plugins: [reactRouter(), tsconfigPaths()],
+    });
+}
+
+export default config;
