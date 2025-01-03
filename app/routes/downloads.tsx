d74b2693b663d266ccbb46d278abce6fa3892abc
Angelillo15 <contact@angelillo15.es>
2025-01-03 11:57:50 +0100

    seo: update meta

--- app/routes/downloads.tsx
+++ app/routes/downloads.tsx
@@ -2,7 +2,7 @@ import { ContentContainer } from '@/components/content-container';
 import type { Route } from './+types/downloads';
 
 export function meta({}: Route.MetaArgs) {
-    return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
+    return [{ title: 'Nookure → Downloads' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
 export default function Home() {
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@ import { Hero } from '@/components/hero';
 import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-    return [{ title: 'Nookure Studios' }, { name: 'description', content: 'Welcome to NookureStudios homepage, ' }];
+    return [{ title: 'Nookure → Home' }, { name: 'description', content: 'Welcome to NookureStudios homepage' }];
 }
 
 export default function Home() {
