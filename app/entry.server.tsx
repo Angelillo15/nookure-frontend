ae7e1699a01ff76781e05a9533fbbb1be4334dc2
Angelillo15 <contact@angelillo15.es>
2025-01-08 23:19:13 +0100

    ssr: try to add cloudflare & prerender at the same time

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,44 +1,70 @@
-/**
- * By default, Remix will handle generating the HTTP Response for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.server
- */
-
 import { ServerRouter, type EntryContext } from 'react-router';
 import { isbot } from 'isbot';
-import { renderToReadableStream } from 'react-dom/server';
 
 const ABORT_DELAY = 5000;
 
-export default async function handleRequest(
+const getDevelopment = () => {
+    if (typeof window !== 'undefined') {
+        return import.meta.env.DEV;
+    } else {
+        return process.env.NODE_ENV === 'development';
+    }
+};
+
+const isWebEnvironment = typeof window !== 'undefined' || getDevelopment();
+
+let server: (request: Request, responseStatusCode: number, responseHeaders: Headers, entryContext: EntryContext) => Promise<Response>;
+
+if (isWebEnvironment) {
+    server = async function handleRequest(
+        request: Request,
+        responseStatusCode: number,
+        responseHeaders: Headers,
+        entryContext: EntryContext
+    ) {
+        const { renderToReadableStream } = await import('react-dom/server');
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), ABORT_DELAY);
+
+        let body = await renderToReadableStream(<ServerRouter context={entryContext} url={request.url} />, {
+            signal: controller.signal,
+            onError(error: unknown) {
+                if (!controller.signal.aborted) {
+                    console.error(error);
+                }
+                responseStatusCode = 500;
+            },
+        });
+
+        body.allReady.then(() => clearTimeout(timeoutId));
+
+        if (isbot(request.headers.get('user-agent') || '')) {
+            await body.allReady;
+        }
+
+        responseHeaders.set('Content-Type', 'text/html');
+        return new Response(body, {
+            headers: responseHeaders,
+            status: responseStatusCode,
+        });
+    };
+} else {
+    server = async function handleRequest(
+        request: Request,
+        responseStatusCode: number,
+        responseHeaders: Headers,
+        entryContext: EntryContext
+    ) {
+        const { default: handleRequestNode } = await import('@/entry.server.node');
+        return handleRequestNode(request, responseStatusCode, responseHeaders, entryContext, {}) as Promise<Response>;
+    };
+}
+
+export default function handleRequest(
     request: Request,
     responseStatusCode: number,
     responseHeaders: Headers,
     entryContext: EntryContext
 ) {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => controller.abort(), ABORT_DELAY);
-
-    const body = await renderToReadableStream(<ServerRouter context={entryContext} url={request.url} />, {
-        signal: controller.signal,
-        onError(error: unknown) {
-            if (!controller.signal.aborted) {
-                // Log streaming rendering errors from inside the shell
-                console.error(error);
-            }
-            responseStatusCode = 500;
-        },
-    });
-
-    body.allReady.then(() => clearTimeout(timeoutId));
-
-    if (isbot(request.headers.get('user-agent') || '')) {
-        await body.allReady;
-    }
-
-    responseHeaders.set('Content-Type', 'text/html');
-    return new Response(body, {
-        headers: responseHeaders,
-        status: responseStatusCode,
-    });
+    return server(request, responseStatusCode, responseHeaders, entryContext);
 }
